e90d583861f69240e533c83e5aa0065d3be02f11
Percy <pdaniel1255@gmail.com>
2024-03-29 03:59:12 -0500

    consultar verify 10

--- SistemaWeb/Js/Consulta.js
+++ SistemaWeb/Js/Consulta.js
@@ -6,7 +6,7 @@ function listar_consulta() {
         
         "ordering": false,
         "bLengthChange": false,
-        "searching": { "regex": true },
+       // "searching": { "regex": true },
         "lenghtMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "ALL"]],
         "pageLenght": 10,
         "destroy": true,

a6c22ded59962cb1c5ad408e81ae32afe09d87aa
Keita <manuel.saucedo@protecso.com.pe>
2024-03-31 16:05:39 -0500

    prueba añadir segundo chart js

--- SistemaWeb/Vista/Dashboard/chartjs.php
+++ SistemaWeb/Vista/Dashboard/chartjs.php
@@ -1,24 +1,24 @@
-
+<?php require_once "../../csscharts/TradingCharts.css"; ?>
     <section class="content">
         <div class="row">
             <!-- /.col (LEFT) -->
             <div class="col-md-12" style="height:100%;">
                 <!-- DONUT CHART -->
                 <div class="box box-danger">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Servicios Prestados</h3>
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-                                    class="fa fa-minus"></i></button>
-                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i
-                                    class="fa fa-times"></i></button>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        <canvas id="pieChart" style="height:250px"></canvas>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
+            <div class="box-header with-border">
+              <h3 class="box-title">Servicios Prestados Por Mes en el Mes Actual a los Pacientes de la Clinica Veterinaria Maria Auxiliadora</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <canvas id="pieChart" style="height:250px"></canvas>
+            </div>
+            <!-- /.box-body -->
+          </div>
                 <!-- /.box -->
                 <!-- <div class="col-md-12" style="height:100%;">
                      
@@ -103,6 +103,162 @@ $(document).ready(function() {
         },
         error: function(error) {
             console.log("Error al obtener datos de servicios:", error);
+        }
+    });
+    $.ajax({
+        url: "../ajaxdashboard/dashboard2.ajax.php",
+        type: 'GET',
+        success: function(respuesta) {
+            console.log("Datos de pacientes obtenidos:", respuesta);
+            var data = JSON.parse(respuesta);
+            var Veces = [];
+            var Mes = [];
+
+            for (var index = 0; index < data.length; index++) {
+                Veces.push(data[index].Veces);
+                Mes.push(data[index].Mes);
+            }
+
+                function createSimpleSwitcher(items, activeItem, activeItemChangedCallback) {
+                var switcherElement = document.createElement('div');
+                switcherElement.classList.add('switcher');
+
+                var intervalElements = items.map(function(item) {
+                    var itemEl = document.createElement('button');
+                    itemEl.innerText = item;
+                    itemEl.classList.add('switcher-item');
+                    itemEl.classList.toggle('switcher-active-item', item === activeItem);
+                    itemEl.addEventListener('click', function() {
+                    onItemClicked(item);
+                    });
+                    switcherElement.appendChild(itemEl);
+                    return itemEl;
+                });
+
+                function onItemClicked(item) {
+                    if (item === activeItem) {
+                    return;
+                    }
+
+                    intervalElements.forEach(function(element, index) {
+                    element.classList.toggle('switcher-active-item', items[index] === item);
+                    });
+
+                    activeItem = item;
+
+                    activeItemChangedCallback(item);
+                }
+
+                return switcherElement;
+                }
+
+                var switcherElement = createSimpleSwitcher(['Dark', 'Light'], 'Dark', syncToTheme);
+
+                var chartElement = document.createElement('div');
+
+                var chart = LightweightCharts.createChart(chartElement, {
+                width: 600,
+                height: 300,
+                rightPriceScale: {
+                    borderVisible: false,
+                },
+                timeScale: {
+                    borderVisible: false,
+                },
+                });
+
+                document.body.appendChild(chartElement);
+                document.body.appendChild(switcherElement);
+
+                var areaSeries = chart.addAreaSeries({
+                topColor: 'rgba(33, 150, 243, 0.56)',
+                bottomColor: 'rgba(33, 150, 243, 0.04)',
+                lineColor: 'rgba(33, 150, 243, 1)',
+                lineWidth: 2,
+                });
+
+                var darkTheme = {
+                chart: {
+                    layout: {
+                    background: {
+                        type: 'solid',
+                        color: '#2B2B43',
+                    },
+                    lineColor: '#2B2B43',
+                    textColor: '#D9D9D9',
+                    },
+                    watermark: {
+                    color: 'rgba(0, 0, 0, 0)',
+                    },
+                    crosshair: {
+                    color: '#758696',
+                    },
+                    grid: {
+                    vertLines: {
+                        color: '#2B2B43',
+                    },
+                    horzLines: {
+                        color: '#363C4E',
+                    },
+                    },
+                },
+                series: {
+                    topColor: 'rgba(32, 226, 47, 0.56)',
+                    bottomColor: 'rgba(32, 226, 47, 0.04)',
+                    lineColor: 'rgba(32, 226, 47, 1)',
+                },
+                };
+
+                const lightTheme = {
+                chart: {
+                    layout: {
+                    background: {
+                        type: 'solid',
+                        color: '#FFFFFF',
+                    },
+                    lineColor: '#2B2B43',
+                    textColor: '#191919',
+                    },
+                    watermark: {
+                    color: 'rgba(0, 0, 0, 0)',
+                    },
+                    grid: {
+                    vertLines: {
+                        visible: false,
+                    },
+                    horzLines: {
+                        color: '#f0f3fa',
+                    },
+                    },
+                },
+                series: {
+                    topColor: 'rgba(33, 150, 243, 0.56)',
+                    bottomColor: 'rgba(33, 150, 243, 0.04)',
+                    lineColor: 'rgba(33, 150, 243, 1)',
+                },
+                };
+
+                var themesData = {
+                Dark: darkTheme,
+                Light: lightTheme,
+                };
+
+                function syncToTheme(theme) {
+                chart.applyOptions(themesData[theme].chart);
+                areaSeries.applyOptions(themesData[theme].series);
+                }
+
+                areaSeries.setData([{
+                    time: Veces,
+                    value: Mes
+                }
+                ]);
+syncToTheme('Dark');
+
+
+        },
+        error: function(error) {
+            console.log("Error al obtener datos de pacientes:", error);
         }
     });
 
